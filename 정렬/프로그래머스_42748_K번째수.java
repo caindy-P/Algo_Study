6703be70dc91549dfa87cc19270c9c4b0c8d4d62
caindy <38023693+caindy-P@users.noreply.github.com>
2026-08-24 13:08:25 +0900

    solve: 프로그래머스 42748 K번째수 - Arrays.copyOfRange로 구간 배열 추출 - Arrays.sort 후 K번째 원소 선택

---
+++ 정렬/프로그래머스_42748_K번째수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution_K번째수 {
+	public int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+		for (int i = 0; i < commands.length; i++) {
+			int[] slice = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+			Arrays.sort(slice);
+			answer[i] = slice[commands[i][2] - 1];
+		}
+		return answer;
+	}
+}
